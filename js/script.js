40924e2beab9c31a1001b5b50251334272aaf3ec
Bradley Dominguez <bradleydominguez25@gmail.com>
2018-03-01 23:15:45 +0000

    Yer

--- js/script.js
+++ js/script.js
@@ -19,28 +19,28 @@
 var mySong = {
 	"title":"24K Magic",
 	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"mp3":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+	"image":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 }
 
 var myPlayList = [
 	{
 		"title":"24K Magic",
 		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"mp3":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
+		"image":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
 	},
 	{
 		"title":"Sir Duke",
 		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"mp3":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
+		"image":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
 	},
 	{
 		"title":"Sorry",
 		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"mp3":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
+		"image":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
 	}
 
 ]
@@ -49,10 +49,14 @@ var myPlayList = [
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-  
-
-
-
+	for(var i=0; i < myPlayList.length; i++){
+   
+    $("body").append("<h1> Title:" + myPlayList[i].title + "</h1>");
+	$("body").append("<h2>Artist:" + myPlayList[i].artist + "</h2>");
+	$("body").append("<a href=" + myPlayList[i].mp3 + ">Song</a>");
+	$("body").append("<img src=" + myPlayList[i].image + ">");
+	}
+	
 });
 
 function displayList(){
